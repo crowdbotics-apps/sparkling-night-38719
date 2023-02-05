7850555a5028d3c0ae760c868e2a92929ff8fc68
Crowdbotics <team@crowdbotics.com>
2023-02-05 15:37:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sparkling-night-38719",
+  url: "https://sparkling-night-38719.botics.co",
+  api: "https://sparkling-night-38719.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
